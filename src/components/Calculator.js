42b464bfed7a0008f1a154c9aeacd93b11706b54
ybadr99 <yousefbadr909@gmail.com>
2023-06-02 00:20:38 +0300

    Add quote componet and fetch quote data from external api

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Button from './Button';
 import calculate from '../logic/calculate';
 import Result from './Result';
+import Quote from './Quote';
 
 const Calculator = () => {
   const [obj, setObj] = useState({});
@@ -11,44 +12,48 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calculator">
-      <Result obj={obj} />
-      <div className="buttons">
-        <Button onClick={clickHandler}>AC</Button>
-        <Button onClick={clickHandler}>+/-</Button>
-        <Button onClick={clickHandler}>%</Button>
-        <Button onClick={clickHandler} className="operation">
-          ÷
-        </Button>
+    <>
+      <div className="calculator">
+        <Result obj={obj} />
+        <div className="buttons">
+          <Button onClick={clickHandler}>AC</Button>
+          <Button onClick={clickHandler}>+/-</Button>
+          <Button onClick={clickHandler}>%</Button>
+          <Button onClick={clickHandler} className="operation">
+            ÷
+          </Button>
 
-        <Button onClick={clickHandler}>7</Button>
-        <Button onClick={clickHandler}>8</Button>
-        <Button onClick={clickHandler}>9</Button>
-        <Button onClick={clickHandler} className="operation">
-          x
-        </Button>
-        <Button onClick={clickHandler}>4</Button>
-        <Button onClick={clickHandler}>5</Button>
-        <Button onClick={clickHandler}>6</Button>
-        <Button onClick={clickHandler} className="operation">
-          -
-        </Button>
+          <Button onClick={clickHandler}>7</Button>
+          <Button onClick={clickHandler}>8</Button>
+          <Button onClick={clickHandler}>9</Button>
+          <Button onClick={clickHandler} className="operation">
+            x
+          </Button>
+          <Button onClick={clickHandler}>4</Button>
+          <Button onClick={clickHandler}>5</Button>
+          <Button onClick={clickHandler}>6</Button>
+          <Button onClick={clickHandler} className="operation">
+            -
+          </Button>
 
-        <Button onClick={clickHandler}>1</Button>
-        <Button onClick={clickHandler}>2</Button>
-        <Button onClick={clickHandler}>3</Button>
-        <Button onClick={clickHandler} className="operation">
-          +
-        </Button>
-        <Button onClick={clickHandler} className="zero">
-          0
-        </Button>
-        <Button onClick={clickHandler}>.</Button>
-        <Button onClick={clickHandler} className="operation">
-          =
-        </Button>
+          <Button onClick={clickHandler}>1</Button>
+          <Button onClick={clickHandler}>2</Button>
+          <Button onClick={clickHandler}>3</Button>
+          <Button onClick={clickHandler} className="operation">
+            +
+          </Button>
+          <Button onClick={clickHandler} className="zero">
+            0
+          </Button>
+          <Button onClick={clickHandler}>.</Button>
+          <Button onClick={clickHandler} className="operation">
+            =
+          </Button>
+        </div>
       </div>
-    </div>
+
+      <Quote />
+    </>
   );
 };
 
