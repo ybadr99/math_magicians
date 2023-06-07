71d48e3532012aac488a9b200c7f854338ef5df5
ybadr99 <yousefbadr909@gmail.com>
2023-06-07 23:55:39 +0300

    make app to full page by using react-router and addin styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <Calculator />;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
