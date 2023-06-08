235e2a6a350c6232f4c3802aa61c7e4b570ef0b3
ybadr99 <yousefbadr909@gmail.com>
2023-06-08 14:56:10 +0300

    add tests for operate module

---
+++ src/tests/operate.test.js
@@ -0,0 +1,20 @@
+import operate from '../logic/operate';
+
+describe('should be able to calculate the number ', () => {
+  it('result of +', () => {
+    const result = operate(1, 2, '+');
+    expect(result).toBe('3');
+  });
+
+  it('result of -', () => {
+    expect(operate(4, 2, '-')).toBe('2');
+  });
+
+  it('result of -', () => {
+    expect(operate(4, 2, '-')).toBe('2');
+  });
+
+  it('result of ÷', () => {
+    expect(operate(4, 2, '÷')).toBe('2');
+  });
+});
